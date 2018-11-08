c97a81e1bb13d7bd705f0304ee72d96171f327b4
AJ <43934638+aaishaalgheilani@users.noreply.github.com>
2018-11-07 23:11:43 -0800

    Update designs.js

--- PAM/project-pixel-art-maker-starter-master/designs.js
+++ PAM/project-pixel-art-maker-starter-master/designs.js
@@ -1,39 +1,37 @@
-var sizePic = document.getElementById("sizePicker");
+const sizePic = document.getElementById("sizePicker");
 const table = document.getElementById("pixelCanvas");
 
+function makeGrid() {
+	//this funciton makes a grid by creating rows based on the height value and creats cells for every row based on the width value
+	var tWidth = document.getElementById("inputWidth").value;
+	var tHeight = document.getElementById("inputHeight").value;
+	for (var x = 0; x < tHeight; x++) {
+		var row = document.createElement("tr");
+		table.appendChild(row);
+		for (var y = 0; y < tWidth; y++) {
+			var cell = document.createElement("td");
+			row.appendChild(cell);
+		}
+	}
+}
 
-sizePic.addEventListener("submit", function makeGrid(event){
-    event.preventDefault();
+function color(cell) {
+//this function is used to color cells based on the value of the colorPicker 
+//and also clears previouslly colored cell
+	var cColor = document.getElementById("colorPicker").value;
+	if (cell.target.style.backgroundColor) {
+		cell.target.style.backgroundColor = null;
+	} else {
+		cell.target.style.backgroundColor = cColor;
+	}
+}
 
-    var tWidth = document.getElementById("inputWidth").value;
-    var tHeight = document.getElementById("inputHeight").value;
+sizePic.addEventListener("submit", function(event) {
+// event listener for cilciking on the submit button it runs makegrid function
+	event.preventDefault(); //prevent refreshing the page after pressng submit
+	table.innerHTML = "";  //clears the  previouse grid 
+	makeGrid();           // this function is called upone clicking  submit
+});
 
-    var cell = document.createElement("td");
-    var row = document.createElement("tr");
-    var cell = document.createElement("td");
-
-    table.innerHTML = "" 
-
-        for (var x = 0 ; x < tHeight; x++){
-            var row = document.createElement("tr");
-            table.appendChild(row);
-
-            for (var y =0 ; y < tWidth; y++){
-                var cell = document.createElement("td");
-                row.appendChild(cell);
-            
-                row.children[y].addEventListener("click", function color(event) {
-                        event.preventDefault();
-
-                        var cColor = document.getElementById("colorPicker").value
-
-                        if (this.style.backgroundColor){  
-                            this.style.backgroundColor = null
-                            
-                        }else{
-                            this.style.backgroundColor = cColor
-                        }
-                })
-            }
-        }
-    })
+table.addEventListener("click", color, false);
+// event listener that react to cilciking on data cells and color them using color function
